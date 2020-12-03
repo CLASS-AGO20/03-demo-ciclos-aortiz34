612e3128c33f512f484aba69f75eade92a7a255b
aortiz34 <aortiz34@ucol.mx>
2020-12-03 14:13:57 -0600

    Added the second function contarImparesFor

--- src/main.js
+++ src/main.js
@@ -9,8 +9,20 @@ export default class App {
         }
         return suma;
     }
+
+    contarImparesFor(numero1, numero2){
+        let contador = 0;
+        for (let i = numero1; i <= numero2; i++) {
+            if (i % 2 !== 0 ){
+                contador = contador + 1;
+            }
+        }
+        return contador;
+    }
 }
 
 let app = new App();
 
 console.log(app.sumarParesFor());
+
+console.log(app.contarImparesFor(12,24));
