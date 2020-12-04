78cd18ea64f8913ec49168d3a18b456ab161deb9
aortiz34 <aortiz34@ucol.mx>
2020-12-03 18:25:21 -0600

    Added the fourth function contarImparesWhile

--- src/main.js
+++ src/main.js
@@ -31,6 +31,18 @@ export default class App {
         }
         return suma;
     }
+
+    contarImparesWhile(inicio, fin){
+        let i = inicio;
+        let contador = 0;
+        while (i <= fin) {
+            if (i % 2 !== 0){
+                contador += 1;
+            }
+            i += 1;
+        }
+        return contador;
+    }
 }
 
 
@@ -41,3 +53,5 @@ console.log(app.sumarParesFor());
 console.log(app.contarImparesFor(12,24));
 
 console.log(app.sumarParesWhile());
+
+console.log(app.contarImparesWhile(12,24));
